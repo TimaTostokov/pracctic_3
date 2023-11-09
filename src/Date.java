ed9551c507fee35d4660743f824cafea0c240c71
TimaTostokov <t1005828@gmail.com>
2023-11-09 10:50:58 +0300

    newclass

---
+++ src/Date.java
@@ -0,0 +1,21 @@
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class Date {
+    public static void main(String[] args) {
+        String firstData = "2022-11-05";
+        String secondData = "2023-11-05";
+
+        LocalDate date1 = LocalDate.parse(firstData);
+        LocalDate date2 = LocalDate.parse(secondData);
+
+        long differenceInDays = ChronoUnit.DAYS.between(date1, date2);
+        long differenceInMonth = ChronoUnit.MONTHS.between(date1, date2);
+        long differenceInYears = ChronoUnit.YEARS.between(date1, date2);
+
+        System.out.println("Разница в днях: " + differenceInDays);
+        System.out.println("Разница в месяцах: " + differenceInMonth);
+        System.out.println("Разница в годах: " + differenceInYears);
+    }
+
+}
